e1b913e4d8570473d5625a6b6ed247cc26811a69
zou <zougengyu@163.com>
2020-04-11 21:42:18 +0800

    修改笔记

---
+++ netty-example/src/main/java/cn/zou/nio/Test.java
@@ -0,0 +1,16 @@
+package cn.zou.nio;
+//import org.junit;
+
+import java.nio.IntBuffer;
+
+public class Test {
+
+//    @Test
+    public void testBuffer() {
+
+    }
+
+    public static void main(String[] args) {
+        IntBuffer allocate = IntBuffer.allocate(0);
+    }
+}
